18e7cfe480cb9e9ac0331fb0ec492b1c8a25fe8a
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-03-18 23:53:29 -0300

    register from 1

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,10 +1,51 @@
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import { api } from "../../services/api";
 import "./Register.sass";
 
-const Register = () => {
+const Register: React.FC = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const name = nameRef.current?.value ?? "";
+    const email = emailRef.current?.value ?? "";
+    const password = passwordRef.current?.value ?? "";
+
+    const response = await api.post("/create-user", { name, email, password });
+    console.log(response);
+  };
+
   return (
-    <>
-      <h1>Register</h1>
-    </>
+    <main className="container">
+      <h1>TO DO LIST</h1>
+      <section className="form-father">
+        <h1>Register</h1>
+        <form className="Form-son" onSubmit={handleFormSubmit}>
+          <div>
+            <label htmlFor="name">Username:</label>
+            <input type="text" name="name" id="name" ref={nameRef} />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" name="email" id="email" ref={emailRef} />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              ref={passwordRef}
+            />
+          </div>
+          <button type="submit">Register</button>
+        </form>
+        <Link to="/login">Already have an account?</Link>
+      </section>
+    </main>
   );
 };
 
