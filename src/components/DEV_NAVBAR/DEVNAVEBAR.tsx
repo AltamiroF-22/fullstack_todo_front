51e0fad72134f8fdf65141b0b7bda8de2820bdbb
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-03-23 23:46:11 -0300

    some small changes

--- src/components/DEV_NAVBAR/DEVNAVEBAR.tsx
+++ src/components/DEV_NAVBAR/DEVNAVEBAR.tsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
-import './sass.sass'
+import "./sass.sass";
 
 const DEVNAVBAR = () => {
   return (
-    <nav>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/">Tasks</Link>
-    </nav>
+    <>
+      <nav className="test-nav">
+        <h4>Nav de testes</h4>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/">Tasks</Link>
+      </nav>
+    </>
   );
 };
 
-
-
-export default DEVNAVBAR
+export default DEVNAVBAR;
